4fc0792f15bcdc597ad81e83d10c92e0dcf09fb2
Anton Holovin <unfeir@gmail.com>
2023-02-12 20:55:31 +0200

    basic

--- basic_main.py
+++ basic_main.py
@@ -8,16 +8,14 @@ import aiohttp
 
 
 parser = argparse.ArgumentParser(description='Currency')
-parser.add_argument('--curr', '-c', required=True, default=['USD', 'EUR'], help='Choose currency')
+parser.add_argument('--curr', '-c', help='Choose currency')
 parser.add_argument('--days', '-d', default=1, help='last _n_ days to show')
 args = vars(parser.parse_args())
-c = args.get('curr')
-d = args.get('days')
+curr = args.get('curr')
+days = args.get('days')
 
 
-def represent(request, cur=[]):
-    currency = ['EUR', 'USD']
-    currency.extend(cur)
+def represent(request, currency):
     result = ''
     result += request['date'] + '\n'
     for i in currency:
@@ -36,8 +34,8 @@ async def request(url):
             async with session.get(url) as resp:
                 logging.debug('request')
                 if resp.status == 200:
-                    txt = await resp.json()
-                    return represent(txt)
+                    data = await resp.json()
+                    return data
 
                 else:
                     logging.error(resp.status)
@@ -46,17 +44,16 @@ async def request(url):
 
 
 def days_to_show(n):
-    if n > 10:
+    if int(n) > 10:
         days = 10
     else:
-        days = n
+        days = int(n)
 
     return [date.today() - timedelta(days=n) for n in range(days)]
 
 
 async def url_build(basicurl, days=1):
     days = days_to_show(days)
-
     for i in days:
         url = basicurl + i.strftime('%d.%m.%Y')
         yield request(url)
@@ -71,12 +68,11 @@ async def main(requests):
 
 if __name__ == '__main__':
     basic_url = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='
-    curr = ['EUR', 'USD']
-    print(curr)
-    print(d, c)
-    # days_to = 3
-    # logging.basicConfig(level=logging.DEBUG)
-    # result = asyncio.run(main(url_build(basic_url, days_to)))
-    # for i in result:
-    #     print(i)
+    basic_cur = ['EUR', 'USD']
+    if curr:
+        basic_cur.append(curr.upper())
+    logging.basicConfig(level=logging.DEBUG)
+    result = asyncio.run(main(url_build(basic_url, days)))
+    for i in result:
+        print(represent(i, basic_cur))
 
